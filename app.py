5e116c0bca9750750af1e6f888186192711c7e2f
William Otieno <jimmywilliamotieno@gmail.com>
2020-05-24 14:55:41 +0300

    Removed bugs

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
+from models import *
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-
     if request.method == 'GET':
         pass
 
@@ -15,6 +18,8 @@ def index():
 
     posts = get_posts()
 
+    return render_template('index.html', posts=posts)
 
-    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,13 +1,16 @@
-import sqlite3
+import sqlite3 as sql
+from os import path
 
 ROOT = path.dirname(path.relpath((__file__)))
+
 
 def create_post(name, content):
     con = sql.connect(path.join(ROOT, 'database.db'))
     cur = con.cursor()
-    cur.execute('insert into posts (name, content) values(? ?)', (name, content))
+    cur.execute('insert into posts (name, content) values(?, ?)', (name, content))
     con.commit()
     con.close()
+
 
 def get_posts():
     con = sql.connect(path.join(ROOT, 'database.db'))
